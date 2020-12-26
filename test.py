9237682ff1a2c9a3efcdaf55a463ebc9082d29f1
PLEXATIC <kim.mueller1999@gmail.com>
2020-12-26 16:43:40 +0100

    removed old testing-stuff

---
+++ test.py
@@ -0,0 +1,13 @@
+class Boye:
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return (self.name)
+    
+boy = Boye("kim")
+print(boy)
+
+Boye.be_fucked = lambda s : print(s.name + " is fucked")
+boy.be_fucked()
+
+import howdoi
